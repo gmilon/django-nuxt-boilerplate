f19cb8b35d9257b430bced9489763875f5878399
guillaume <gui@mixfame.com>
2020-10-08 18:30:02 +0400

    add missing files..

---
+++ front-end/layouts/public.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
---
+++ front-end/pages/todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <v-input v-model="login"></v-input>
+      <v-input v-model="password"></v-input>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  data() {
+    return {
+      login: null,
+      password: null,
+    }
+  },
+  methods: {
+    loggin() {},
+  },
+}
+</script>
